522ac7f1b4a34e07090c6fbb576b5cfedfce02fc
KenanMehmet <kenmehmet1@gmail.com>
2022-09-15 10:53:13 +0100

    saving inital model

--- main_site/models.py
+++ main_site/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Client(models.Model):
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('non_binary', 'Non Binary'),
+        ()
+    )
+    forename = models.CharField(max_length=24)
+    surname = models.CharField(max_length=24)
+    created_on = models.DateTimeField(default=timezone.now)
+    gender = models.CharField(choices=GENDER_CHOICES)
